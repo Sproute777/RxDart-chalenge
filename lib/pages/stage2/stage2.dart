8127d9c486bbe2bb26c8f8f77effbc0621f0e239
Dmitry Bannikov <prestiz4321@gmail.com>
2021-11-12 21:39:21 +0300

    stage2

---
+++ lib/pages/stage2/stage2.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class Stage2 extends StatelessWidget {
+  const Stage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
